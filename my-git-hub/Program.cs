d690a165c9a45d584450e099e79f42d50a868687
Petr Herian <petr.herian@avast.com>
2022-04-10 10:36:20 +0200

    added project for write/read data from/to xml file

--- my-git-hub/Program.cs
+++ my-git-hub/Program.cs
@@ -5,30 +5,28 @@ class Program
     public static Branch currentBranch;
     static void Main(string[] args)
     {
-        Console.WriteLine("Ahoj");
-        XMLWrite.WriteXML(new User("Petr"));
-        // //STARTING APP WITH FEW PRINTS AND INPUT
-        // Console.WriteLine("Welcome on GitHub");
-        // System.Threading.Thread.Sleep(500);
-        // Console.WriteLine("Enter your name:");
-        // string inputName = Console.ReadLine();
-        // //TODO CHECK IF USER EXISTS AND LOAD DATA FROM XML FILE
-        // User user = new User(inputName);
-        // //CREATE MASTER BRANCH FOR NEW USER
-        // user.addBranch("master");
-        // TimeClass.loadingFunction("Creating Github account");
-        // Boolean flag = true;
-        // currentBranch = (Branch)user.listOfBranch[0];
-        // //APP
-        // while (flag)
-        // {
-        //     Console.WriteLine("USER: " + user.name + ", BRANCH: " + currentBranch.name);
-        //     Functions.basicInfo();
-        //     string input = Console.ReadLine();
-        //     Console.Clear();
-        //     currentBranch = Functions.signpost(input, user, currentBranch);
-        //     Console.Clear();
-        // }
-        // Environment.Exit(0);
+        //STARTING APP WITH FEW PRINTS AND INPUT
+        Console.WriteLine("Welcome on GitHub");
+        System.Threading.Thread.Sleep(500);
+        Console.WriteLine("Enter your name:");
+        string inputName = Console.ReadLine();
+        //TODO CHECK IF USER EXISTS AND LOAD DATA FROM XML FILE
+        User user = new User(inputName);
+        //CREATE MASTER BRANCH FOR NEW USER
+        user.addBranch("master");
+        TimeClass.loadingFunction("Creating Github account");
+        Boolean flag = true;
+        currentBranch = (Branch)user.listOfBranch[0];
+        //APP
+        while (flag)
+        {
+            Console.WriteLine("USER: " + user.name + ", BRANCH: " + currentBranch.name);
+            Functions.basicInfo();
+            string input = Console.ReadLine();
+            Console.Clear();
+            currentBranch = Functions.signpost(input, user, currentBranch);
+            Console.Clear();
+        }
+        Environment.Exit(0);
     }
 }
---
+++ write-read-file/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Xml;
+
+System.Console.WriteLine("Start App");
+string[] branches = { "Volvo", "BMW", "Ford", "Mazda" };
+string[] users = { "Petr", "Ondra", "Marek", "Tomas" };
+
+writeToFile(branches, users);
+readFromFile();
+
+static void writeToFile(string[] branches, string[] users)
+{
+    Console.WriteLine("Writing, to file!");
+
+    XmlWriterSettings set = new XmlWriterSettings();
+    set.Indent = true; //nastaveni odrazeni
+
+    using (XmlWriter xw = XmlWriter.Create(@"users.xml", set))
+    {
+        //Zalozeni dokumentu??
+        xw.WriteStartDocument();
+        xw.WriteStartElement("users");
+        for (int i = 0; i < users.Length; i++)
+        {
+
+            //pridani hodnot do dokumentu
+            xw.WriteStartElement("user");
+            xw.WriteAttributeString("username", users[i]);
+            xw.WriteEndElement();
+            xw.WriteStartElement("branches");
+            for (int j = 0; j < branches.Length; j++)
+            {
+                xw.WriteStartElement("branch");
+                xw.WriteAttributeString("branchName", branches[j]);
+                xw.WriteEndElement();
+            }
+            xw.WriteEndElement();
+
+        }
+        xw.WriteEndElement();
+        xw.WriteEndDocument();
+        xw.Flush();
+    }
+}
+
+static void readFromFile()
+{
+    using (XmlReader xr = XmlReader.Create(@"users.xml"))
+    {
+        int body;
+        while (xr.Read())
+        {
+            if (xr.NodeType == XmlNodeType.Element)
+            {
+                if (xr.GetAttribute("username") != null)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(xr.GetAttribute("username"));
+                    Console.WriteLine("Branches: ");
+                }
+                if (xr.GetAttribute("branchName") != null)
+                {
+                    Console.WriteLine(xr.GetAttribute("branchName"));
+                }
+            }
+        }
+    }
+}
